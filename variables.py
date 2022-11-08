74ab5fde06d1fba37763818956339086f0ba6808
rharshitaaa21 <raghav.harshita21@gmail.com>
2022-11-08 17:47:22 +0530

    Variables

---
+++ variables.py
@@ -0,0 +1,23 @@
+x = 10 
+print(x)
+y = 6 
+print(y)
+print( x+ y)
+name = 'Harshita'
+print(name)
+print( name + 'Raghav')
+print( name + ' Raghav')
+print( name[0]+name[1]+name[2]+name[3])
+
+'''
+indexing form front -> 0,1,2,3,4,5,6,7....
+indexing form end ->   ...-7,-6,-5,-4,-3,-2,-1
+
+'''
+val = 'Modinagar'
+print(val[0])
+print ( val[1])
+print(val[1:4])
+print( val[1:])
+print(val[:4])
+ #strings in immutable
