42b070c209540d39da00b8592696e9d2aabf6133
rharshitaaa21 <raghav.harshita21@gmail.com>
2022-11-12 22:07:48 +0530

    Tuples and Variables

---
+++ tuples.py
@@ -0,0 +1,6 @@
+tup = (12,24,36,48,60)
+print(tup[1])
+print(tup.count(36))
+s={22,25,6,26,27,16,28}
+
+print(s) 
--- variables.py
+++ variables.py
@@ -11,7 +11,7 @@ print( name[0]+name[1]+name[2]+name[3])
 
 '''
 indexing form front -> 0,1,2,3,4,5,6,7....
-indexing form end ->   ...-7,-6,-5,-4,-3,-2,-1
+indexing form end ->   ...-7,-6,-5,-4,-3,-2,-1 
 
 '''
 val = 'Modinagar'
@@ -20,4 +20,30 @@ print ( val[1])
 print(val[1:4])
 print( val[1:])
 print(val[:4])
- #strings in immutable
+ #strings are immutable
+'''
+ num = 5
+id(num)
+140724565103528
+name= 'Dee'
+id(name)
+2401270323120
+a= 10   // address-1 assigned
+b = 12   //address-2 assigned
+ans = a+b   //address-3 assigned
+print( ans )
+22
+id(a)
+140724565103688
+id(b)
+140724565103752
+id(ans)
+140724565104072
+x = 10   //add-1 assigned
+y = 10 //add-2 assigned , but add-2 == add-2 ,when same values are declared same add is given to both the variables 
+id(x)
+140724565103688
+id(y)
+140724565103688
+
+ '''
