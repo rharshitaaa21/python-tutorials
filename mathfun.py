239e6d605a8ab0183a095c99257062a4cd37a770
rharshitaaa21 <raghav.harshita21@gmail.com>
2022-11-14 23:05:53 +0530

    Maths

---
+++ mathfun.py
@@ -0,0 +1,17 @@
+import math #method-1
+
+x = math.sqrt(25)
+print(x)
+
+print(math.sqrt(3))  
+print(math.floor(34534.3487837847))
+print(math.ceil(34534.56))
+print(math.pow(3,2))
+
+
+import math as m      #method-2
+print(m.factorial(5))
+
+
+from math import sqrt, floor, ceil #method-3
+print(sqrt(72))
---
+++ operators.py
@@ -0,0 +1,16 @@
+x= 10
+y=20
+z=30
+print(x+y+z)
+print(x-y-z)
+print(x*y*z)
+print(x%y)
+
+a, b = 10,20
+print(a)
+print(b)
+print(a<10 and b>10)
+
+#numbersystem
+print(bin(7))
+print(bin(25))
