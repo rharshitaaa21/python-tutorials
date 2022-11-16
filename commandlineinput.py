793a5e57cd2adda8f2fa4a845a37c103b82d93bc
rharshitaaa21 <raghav.harshita21@gmail.com>
2022-11-16 12:58:15 +0530

    CommandLineInput

---
+++ commandlineinput.py
@@ -0,0 +1,18 @@
+#commandlineinputs ---------------------------------------
+
+# x = int(input("Enter A "))
+# y = int(input("Enter B "))
+# z= x*y
+# print( z)
+
+#user input stores everything into string so you need to convert everything 
+#input will accept and entire string 
+
+# x = int(input("Enter A"))
+# y = int(input("Enter B"))
+# z = x+y
+# print(z)
+
+
+res = eval(input("enter an expression"))
+print(res)
