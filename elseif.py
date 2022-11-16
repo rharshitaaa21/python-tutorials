ea2ee1cc87786a6dd983c389d82e5fd5f1af46a9
rharshitaaa21 <raghav.harshita21@gmail.com>
2022-11-16 13:21:49 +0530

    ElseIf

---
+++ elseif.py
@@ -0,0 +1,22 @@
+x = 10 
+y = 25 
+z = 39
+
+res = x-y+z
+if( res <50):
+    print("I was right")
+else:
+    print("Oopise, I was wrong")
+
+a = 5 
+if( a == 1):
+    print("one")
+
+elif(a ==2): 
+    print("two")
+
+elif(a == 3):
+    print("three") 
+
+else:
+    print("Wrong input")
